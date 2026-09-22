cc9353f8d4aa5bfbdfd6e578a1331a783c154d1a
nywm8rmpf4-sketch <nywm8rmpf4@privaterelay.appleid.com>
2026-09-22 16:26:41 +0200

    fix(r11): src/pwa/sw.ts

--- src/pwa/sw.ts
+++ src/pwa/sw.ts
@@ -6,4 +6,7 @@ declare let self: ServiceWorkerGlobalScope & { __WB_MANIFEST: Array<{ url: strin
 
 cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
+self.addEventListener('message', (event) => {
+  if (event.data?.type === 'SKIP_WAITING') void self.skipWaiting()
+})
 clientsClaim()
